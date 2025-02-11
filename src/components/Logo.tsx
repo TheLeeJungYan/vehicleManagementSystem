2e96c9f954b8bfb88a557537b6d42dcd3baad0fd
leejungyan08 <jungyan.lee@polyware.com.my>
2025-02-11 14:19:34 +0800

    update header

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,11 +1,16 @@
-const Logo:React.FC = () => {
-    return (
-        <div className="flex">
-            <div className="size-14 rounded-xl bg-radial-[at_25%_25%] from-blue-200 to-blue-600 to-75%">
-
-            </div>
-        </div>
-    )
-}
+import logo from "@/assets/bus.png";
+const Logo: React.FC = () => {
+  return (
+    <div className="flex font-inter gap-5 items-center font-bold text-2xl">
+      <div className="size-16 py-2 px-3 rounded-2xl bg-gray-100 border flex items-center justify-center text-white">
+        <img src={logo} alt="" className="object-contain h-14" />
+      </div>
+      <span className="">
+        <span className="">Vee</span>
+        Manage
+      </span>
+    </div>
+  );
+};
 
 export default Logo;
