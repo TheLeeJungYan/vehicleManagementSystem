05fbd1a0e900b0a41c3fa835d32b0605c5bb4f6a
TheLeeJungYan <jungyan.lee@gmail.com>
2025-02-14 21:34:13 +0800

    update

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import { resolve } from "path";
-import commonjs from '@rollup/plugin-commonjs';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    react(),tailwindcss(),commonjs()
+    react(),tailwindcss()
   ],
   resolve: {
     alias: {
